b8759fd45dd918d087de37251f36fd02b6363ebd
dongsu-iis <dongsu.workout@outlook.jp>
2018-08-29 21:21:42 +0900

    Part1 ASP.NET MVC プロジェクト階層化アーキテクチャ

---
+++ SampleMvc_Part1/SampleMvc_Web/Models/Interface/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace SampleMvc_Web.Models.Interface
+{
+    interface ICategoryRepository : IDisposable
+    {
+        void Create(Category instance);
+
+        void Update(Category instance);
+
+        void Delete(Category instance);
+
+        Category Get(int categoryId);
+
+        IQueryable<Category> GetAll();
+
+        void SaveChanges();
+    }
+}
---
+++ SampleMvc_Part1/SampleMvc_Web/Models/Interface/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace SampleMvc_Web.Models.Interface
+{
+    interface IProductRepository : IDisposable
+    {
+        void Create(Product instance);
+
+        void Update(Product instance);
+
+        void Delete(Product instance);
+
+        Product Get(int productId);
+
+        IQueryable<Product> GetAll();
+
+        void SaveChanges();
+    }
+}
